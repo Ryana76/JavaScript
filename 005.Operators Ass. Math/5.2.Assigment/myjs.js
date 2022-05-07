b5ca3ee92f41c1c115b697ba57130e7a1945a364
ryana <ryana150876@gmail.com>
2022-05-07 15:22:24 +0200

    Add folder assigmant

---
+++ 005.Operators Ass. Math/5.2.Assigment/myjs.js
@@ -0,0 +1,7 @@
+let num = 10;
+document.getElementById("ex1").innerHTML = "(num += 5) : " + (num += 5) + "<br>" + "num = " + num;
+document.getElementById("ex2").innerHTML = "(num -= 5) : " + (num -= 5) + "<br>" + "num = " + num;
+document.getElementById("ex3").innerHTML = "(num *= 5) : " + (num *= 5) + "<br>" + "num = " + num;
+document.getElementById("ex4").innerHTML = "(num /= 5) : " + (num /= 5) + "<br>" + "num = " + num;
+document.getElementById("ex5").innerHTML = "(num %= 5) : " + (num %= 5) + "<br>" + "num = " + num;
+document.getElementById("ex6").innerHTML = "(num += 5) : " + (num += 5) + "<br>" + "num = " + num;
