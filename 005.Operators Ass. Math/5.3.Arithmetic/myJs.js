597192744eb0b3068d28098c9b19c9b703f636cf
ryana <ryana150876@gmail.com>
2022-05-07 15:54:28 +0200

    Add file

---
+++ 005.Operators Ass. Math/5.3.Arithmetic/myJs.js
@@ -0,0 +1,4 @@
+let num = 5;
+let num2 = num ** 3;
+document.getElementById("ex1").innerHTML = "5 ^ 3 =" + num2;
+document.getElementById("ex2").innerHTML = "Usare <br> Math.pow(5, 3) = " + Math.pow(num, 3);
